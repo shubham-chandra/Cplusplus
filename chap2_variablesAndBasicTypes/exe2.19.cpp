60c07fcbc082f125b65bafe84e5df80f6bbad1b6
shubham chandra <shubhamchandra317@gmail.com>
2020-08-29 14:59:49 +0530

    Add solution to exercise 2.19 and 2.20

---
+++ chap2_variablesAndBasicTypes/exe2.19.cpp
@@ -0,0 +1,15 @@
+
+
+
+
+
+#include <iostream>
+
+int i = 12;
+int main(){
+	int i = 42;
+	int j = i ;
+
+	std::cout << j <<std::endl;
+	// Prints 42
+}
---
+++ chap2_variablesAndBasicTypes/exe2.20.cpp
@@ -0,0 +1,17 @@
+
+
+
+
+
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+	int i = 100,sum = 0;
+
+	for (int i = 0; i < 10; ++i)
+		sum+=i;
+	std::cout << i << " " << sum << std::endl;
+	//print i = 100 and sum = 45
+	return 0;
+}
